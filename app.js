261026fc925a8b45cce5fc83bccfe7f1cda5d174
Alisson Pinto <alisson.pinto@outlook.com>
2016-04-23 01:00:03 -0300

    Tela de Login

---
+++ app.js
@@ -0,0 +1,26 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
